301c46891f453ff53b9fa36f718c92c0b980cc32
LiuJH <2294272131@qq.com>
2021-05-12 10:39:32 +0800

    v

--- src/main/java/cn/edu/ncepu/researchplatform/config/WebMvcConfig.java
+++ src/main/java/cn/edu/ncepu/researchplatform/config/WebMvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
     }
 }
