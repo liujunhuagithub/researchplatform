1a826e4330d1b94431cb6a788a5638a28de86988
LiuJH <2294272131@qq.com>
2021-05-12 17:19:40 +0800

    x

---
+++ src/main/java/cn/edu/ncepu/researchplatform/common/R.java
@@ -0,0 +1,52 @@
+package cn.edu.ncepu.researchplatform.common;
+
+import cn.edu.ncepu.researchplatform.common.exception.CustomException;
+
+public class R {
+    private int code;
+    private String message;
+    private Object data;
+
+    public static R fail(int code, String meaasge) {
+        return new R(code, meaasge,null);
+    }
+
+    public static R success(Object data) {
+        return new R(200, null, data);
+    }
+
+    public static R fail(CustomException exception) {
+        return new R(exception.getCode(), exception.getMessage(), null);
+    }
+
+    private R(int code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
