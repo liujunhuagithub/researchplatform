26ec99e5addd561bb814d6631707a5d1b75facca
LiuJH <2294272131@qq.com>
2021-05-13 10:27:52 +0800

    v

--- src/main/groovy/cn/edu/ncepu/researchplatform/config/SecurityConfig.java
+++ src/main/groovy/cn/edu/ncepu/researchplatform/config/SecurityConfig.java
@@ -2,8 +2,14 @@ package cn.edu.ncepu.researchplatform.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true,proxyTargetClass = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().anonymous();
+    }
 }
