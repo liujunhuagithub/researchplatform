d92cfe012baaf478c18d79218dbb77b423e40670
LiuJH <2294272131@qq.com>
2021-05-15 09:11:32 +0800

    v

--- src/main/groovy/cn/edu/ncepu/researchplatform/service/PeopleService.java
+++ src/main/groovy/cn/edu/ncepu/researchplatform/service/PeopleService.java
@@ -4,13 +4,17 @@ import cn.edu.ncepu.researchplatform.mapper.PeopleMapper;
 import cn.edu.ncepu.researchplatform.security.PeopleDetails;
 import cn.hutool.core.util.PhoneUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
+@CacheConfig(cacheNames = "people")
 public class PeopleService {
     @Autowired
     private PeopleMapper peopleMapper;
 
+    @Cacheable(key = "#username")
     public PeopleDetails findByUsername(String username) {
         return peopleMapper.findByUsername(Integer.parseInt(username));
     }
