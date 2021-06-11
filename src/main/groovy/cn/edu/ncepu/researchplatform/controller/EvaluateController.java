46df0988dd0bfb699178d897dd0c11b667a739a7
LiuJH <2294272131@qq.com>
2021-06-11 11:06:08 +0800

    v

--- src/main/groovy/cn/edu/ncepu/researchplatform/controller/EvaluateController.java
+++ src/main/groovy/cn/edu/ncepu/researchplatform/controller/EvaluateController.java
@@ -6,6 +6,7 @@ import cn.edu.ncepu.researchplatform.service.PeopleService;
 import cn.edu.ncepu.researchplatform.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -14,6 +15,7 @@ import java.util.Map;
 public class EvaluateController {
     @Autowired
     private EvaluateService evaluateService;
+    private PeopleService peopleService;
 
     //    @PostAuthorize("#username==#authentication.name or hasAuthority('admin')")
     @DeleteMapping("/people/{username}/evaluate/{evaluateId}")
@@ -22,10 +24,13 @@ public class EvaluateController {
     }
 
     @PutMapping("/evaluate/{id}/adopt")
+    @Transactional(rollbackFor = Exception.class)
     public boolean 置顶采纳evaluate(@PathVariable Integer id, @RequestBody Map<String, Integer> params) {
         Integer flag = params.get("flag");
         if (flag.equals(-1)) throw CustomException.INPUT_ERROE_Exception;
-        if (!evaluateService.getAuthor(id).getUsername().equals(Utils.getCurrent())) throw CustomException.AUTH_ERROR_Exception;
+        if (!evaluateService.getAuthor(id).getUsername().equals(Utils.getCurrent()))
+            throw CustomException.AUTH_ERROR_Exception;
+        peopleService.cost(id, 10);
         return evaluateService.updateFlag(id, params.get("flag"));
     }
 }
