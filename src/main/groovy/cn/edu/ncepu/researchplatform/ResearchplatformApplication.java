08ddcb3606ca08f0f58076383a827ddab1bbab7a
LiuJH <2294272131@qq.com>
2021-09-10 20:47:44 +0800

    致命打击

--- src/main/groovy/cn/edu/ncepu/researchplatform/ResearchplatformApplication.java
+++ src/main/groovy/cn/edu/ncepu/researchplatform/ResearchplatformApplication.java
@@ -1,20 +1,12 @@
 package cn.edu.ncepu.researchplatform;
 
-import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.lang.tree.Tree;
-import cn.hutool.core.lang.tree.TreeNode;
-import cn.hutool.core.lang.tree.TreeNodeConfig;
-import cn.hutool.core.lang.tree.TreeUtil;
-import cn.hutool.core.lang.tree.parser.DefaultNodeParser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.List;
 
 @EnableScheduling
 @EnableAsync
